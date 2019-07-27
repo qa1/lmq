2820514b4becd140afe1b594d75ad992cff08c87
Misam <misamplus@gmail.com>
2019-07-27 16:29:57 +0430

    Move python lib to a new repository.

--- libs/python/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import version
-from .LightweightMessageQueueLibrary import LightweightMessageQueueLibrary
-
-__version_info__ = version.__version_info__
-__version__ = version.__version__
--- libs/python/version.py
+++
@@ -1,4 +0,0 @@
-# Versions should comply with PEP440.
-# This line is parsed in setup.py:
-__version__ = '1.2.2'
-__version_info__ = tuple([int(i) for i in __version__.split('.')])
